5bd37619baeeebd4a43c64027971b8b0974bb4b7
Xavier Lacombe <xavierlac@MacBook-Pro-de-Xavier.local>
2019-10-14 16:50:17 +0900

    Outputting the orders

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -2,11 +2,39 @@ import React from 'react';
 import classes from './Order.module.css';
 
 const Order = props => {
+  const ingredients = [];
+
+  for (let ingredientName in props.ingredients) {
+    console.log(props.ingredients[ingredientName]);
+    ingredients.push({
+      name: ingredientName,
+      // Quantité de chaque ingrédient
+      amount: props.ingredients[ingredientName]
+    });
+  }
+
+  const ingredientOutput = ingredients.map(ingredient => {
+    return (
+      <span
+        style={{
+          textTransform: 'capitalize',
+          display: 'inline-block',
+          margin: '0 8px',
+          border: '1px solid #ccc',
+          padding: '5px'
+        }}
+        key={ingredient.name}
+      >
+        {ingredient.name} ({ingredient.amount})
+      </span>
+    );
+  });
   return (
     <div className={classes.Order}>
-      <p>Ingredients: Salad (1)</p>
+      <p>Ingredients: {ingredientOutput}</p>
       <p>
-        Price: <strong>USD 5.45</strong>
+        {/* Number.parseFloat car props.price est un string. Si on veut appliquer toFixed(2), il faut au préalable convertir en nombre */}
+        Price: <strong>USD {Number.parseFloat(props.price).toFixed(2)}</strong>
       </p>
     </div>
   );
