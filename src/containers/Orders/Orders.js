dc8b76ed985ce7520fd4547e9d9ef42d6aadc29c
Xavier Lacombe <xavierlac@MacBook-Pro-de-Xavier.local>
2019-10-14 16:35:59 +0900

    Fetching orders from firebase

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,6 +1,32 @@
 import React from 'react';
 import Order from '../../components/Order/Order';
+import axios from '../../axios-orders';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
+
 class Orders extends React.Component {
+  state = {
+    orders: [],
+    loading: true
+  };
+  componentDidMount() {
+    axios
+      .get('/orders.json')
+      .then(response => {
+        // On crée un nouvel objet avec le contenu de response.data et on y ajoute un ID qui correspond à la clé générée par Firebase
+        const fetchedOrders = [];
+        for (let key in response.data) {
+          fetchedOrders.push({
+            ...response.data[key],
+            id: key
+          });
+        }
+        this.setState({ loading: false, orders: fetchedOrders });
+      })
+      .catch(err => {
+        this.setState({ loading: false });
+      });
+  }
+
   render() {
     return (
       <div>
@@ -11,4 +37,4 @@ class Orders extends React.Component {
   }
 }
 
-export default Orders;
+export default withErrorHandler(Orders, axios);
